6bfd8472c95b37609cc229930876448f46f7509d
nghiant <nghiant2@vmogroup.com>
2025-02-27 22:59:49 +0700

    security default

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,44 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    private Long id;
+    private String username;
+    private String password;
+    private String role;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
